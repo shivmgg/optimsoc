2d2907225d5e112df2c74a3bc74875de36cde1a8
Philipp Wagner <philipp.wagner@tum.de>
2017-04-28 10:19:31 +0200

    WIP: Add cocotb testrunner
    Currently only working for VCS. I'm not yet happy with the place of the
    code and its dependencies, this needs to be sorted out first before
    getting it into master.

---
+++ test/cocotb/cocotb_testrunner.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python3
+
+import os
+import yaml
+import sys
+import glob
+from optparse import OptionParser
+import subprocess
+
+tests = []
+objdir = None
+
+class CocotbTest():
+    def __init__(self, manifest_path):
+        self._manifest_path = None
+        self.manifest = None
+        self.objdir = os.path.join(os.getcwd(), 'objdir')
+        
+        self.manifest_path = manifest_path
+        
+    def _abspath(self, path, basedir):
+        if path.startswith('/'):
+            return path
+        return os.path.abspath(os.path.join(basedir, path))
+        
+    def _parse_manifest(self, manifest_path):
+        with open(manifest_path, 'r') as fp_manifest:
+            try:
+                manifest = yaml.safe_load(fp_manifest)
+            except yaml.YAMLError as e:
+                print(e)
+                return False
+
+        # ensure absolute paths in the list of sources
+        manifest_dir = os.path.dirname(manifest_path)
+        manifest['sources'][:] = map(lambda x: self._abspath(x, manifest_dir), 
+                                     manifest['sources'])            
+            
+        manifest['manifest_dir'] = os.path.abspath(manifest_dir) 
+        return manifest
+
+    @property
+    def manifest_path(self):
+        return self._manifest_path
+    
+    @manifest_path.setter
+    def manifest_path(self, manifest_path):
+        self.manifest = self._parse_manifest(manifest_path)
+        self._manifest_path = manifest_path
+
+    def _get_makefile_vcs(self):
+        makefile = "# Auto-generated Makefile by OpTiMSoC for Synopsys VCS\n"
+        makefile += "SIM=vcs\n"
+        makefile += "VERILOG_SOURCES=" + " \\\n\t".join(self.manifest["sources"]) + "\n"
+        makefile += "TOPLEVEL=" + self.manifest["toplevel"] + "\n"
+        makefile += "MODULE=" + self.manifest["module"] + "\n"
+        
+        # HDL parameters (passed to toplevel module in the design)
+        args_hdl_params = []
+        for name, value in self.manifest["parameters"].items():
+            args_hdl_params.append("-pvalue+{}={}".format(name, value))
+
+        #        sim_args = " ".join(args_hdl_params)
+        sim_args = "" 
+        compile_args = "-timescale=1ns/10ps " + " ".join(args_hdl_params) 
+
+        makefile += "SIM_ARGS=" + sim_args + "\n"
+        makefile += "COMPILE_ARGS=" + compile_args + "\n"
+
+        makefile += "include $(COCOTB)/makefiles/Makefile.inc\n"
+        makefile += "include $(COCOTB)/makefiles/Makefile.sim\n"
+
+        return makefile
+    
+    def _prepare_objdir(self):
+        # create Makefile
+        makefile_contents = self._get_makefile_vcs()
+        with open("{}/Makefile".format(self.objdir), "w") as fp_makefile:
+            fp_makefile.write(makefile_contents)
+    
+    def run(self):
+        self._prepare_objdir()
+        
+        env = os.environ
+        env['PYTHONPATH'] = self.manifest['manifest_dir']
+        subprocess.run("make", cwd=self.objdir, env=env)
+        
+
+
+def run_tests(tests):
+    for t in tests:
+        t.run()
+
+
+def discover_tests(test_search_base):
+    for f in glob.iglob('{}/**/manifest.yaml'.format(test_search_base), recursive=True):
+        cocotb_test = CocotbTest(manifest_path=f)
+        cocotb_test.objdir = objdir
+        tests.append(cocotb_test)
+
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-o", "--objdir", dest="objdir",
+                      help="object directory [default: %default]",
+                      default=os.path.join(os.getcwd(), "objdir"))
+
+    (options, args) = parser.parse_args()
+    
+    if len(args) < 1:
+        test_search_base = os.getcwd()
+    else:
+        test_search_base = args[0];
+        
+    if not os.path.isdir(test_search_base):
+        printf("Specified search base not an existing directory.")
+        sys.exit(1)
+
+    objdir = options.objdir;
+
+    discover_tests(test_search_base)
+    run_tests(tests)
+
