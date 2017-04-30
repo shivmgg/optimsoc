394cdd60dd09929f2885a565acddf85266e6fc95
Philipp Wagner <philipp.wagner@tum.de>
2017-04-30 17:58:21 +0200

    WIP: Improve testrunner
    Now tests can also be started with the --gui option

--- test/cocotb/cocotb_testrunner.py
+++ test/cocotb/cocotb_testrunner.py
@@ -4,7 +4,7 @@ import os
 import yaml
 import sys
 import glob
-from optparse import OptionParser
+import argparse
 import subprocess
 
 tests = []
@@ -61,8 +61,8 @@ class CocotbTest():
             args_hdl_params.append("-pvalue+{}={}".format(name, value))
 
         #        sim_args = " ".join(args_hdl_params)
-        sim_args = "" 
-        compile_args = "-timescale=1ns/10ps " + " ".join(args_hdl_params) 
+        sim_args = "+lint=all" 
+        compile_args = "+lint=all -timescale=1ns/10ps " + " ".join(args_hdl_params) 
 
         makefile += "SIM_ARGS=" + sim_args + "\n"
         makefile += "COMPILE_ARGS=" + compile_args + "\n"
@@ -78,18 +78,23 @@ class CocotbTest():
         with open("{}/Makefile".format(self.objdir), "w") as fp_makefile:
             fp_makefile.write(makefile_contents)
     
-    def run(self):
+    def run(self, gui, loglevel='INFO'):
         self._prepare_objdir()
         
         env = os.environ
         env['PYTHONPATH'] = self.manifest['manifest_dir']
-        subprocess.run("make", cwd=self.objdir, env=env)
+        env['COCOTB_LOG_LEVEL'] = loglevel
+        subprocess.run(["make", "SIM_ARGS=-gui"], cwd=self.objdir, env=env)
         
 
 
-def run_tests(tests):
+def run_tests(tests, gui=False, loglevel='INFO'):
+    if len(tests) > 1:
+        printf("You cannot run multiple tests with GUI. Please run again with a single test.")
+        exit(1)
+
     for t in tests:
-        t.run()
+        t.run(gui, loglevel)
 
 
 def discover_tests(test_search_base):
@@ -100,24 +105,27 @@ def discover_tests(test_search_base):
 
 
 if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option("-o", "--objdir", dest="objdir",
-                      help="object directory [default: %default]",
-                      default=os.path.join(os.getcwd(), "objdir"))
+    parser = argparse.ArgumentParser(description='Testrunner for cocotb testbenches')
+    parser.add_argument("-o", "--objdir", 
+                        help="object directory [default: %(default)s]",
+                        default=os.path.join(os.getcwd(), "objdir"))
+    parser.add_argument("-l", "--loglevel", 
+                        help="cocotb log level [default: %(default)s]",
+                        default='INFO')
+    parser.add_argument("-g", "--gui", action='store_true',
+                        help="show GUI[default: %(default)s]",
+                        default=False)
+    parser.add_argument('dir', nargs='?', default=os.getcwd())
 
-    (options, args) = parser.parse_args()
-    
-    if len(args) < 1:
-        test_search_base = os.getcwd()
-    else:
-        test_search_base = args[0];
-        
+    args = parser.parse_args()
+
+    objdir = args.objdir
+    test_search_base = args.dir
+                
     if not os.path.isdir(test_search_base):
         printf("Specified search base not an existing directory.")
         sys.exit(1)
 
-    objdir = options.objdir;
 
     discover_tests(test_search_base)
-    run_tests(tests)
-
+    run_tests(tests, gui=args.gui, loglevel=args.loglevel)
